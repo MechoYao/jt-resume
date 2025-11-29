ef95265ceb1a8b3c22acba2264d0c601e919caee
Mecho <i@mecho.cc>
2025-11-29 13:31:52 +0800

    Fix path

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-const repoName = 'jt-resume'; // 你的 GitHub 仓库名
+// const isProd = process.env.NODE_ENV === 'production';
+// const repoName = ''; // 你的 GitHub 仓库名
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: isProd ? `/${repoName}` : '',
-  assetPrefix: isProd ? `/${repoName}/` : '',
+  // basePath: isProd ? `/${repoName}` : '',
+  // assetPrefix: isProd ? `/${repoName}/` : '',
   images: {
     unoptimized: true,
   },
